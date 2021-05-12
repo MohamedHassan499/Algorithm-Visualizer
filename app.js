11c06a53886c6e0091273a0beb664428416ec52b
MohamedHassan499 <50884619+MohamedHassan499@users.noreply.github.com>
2021-05-12 13:46:35 +0200

    Color cells as blocked

--- app.js
+++ app.js
@@ -1,187 +1,304 @@
-function colorCell(event){
-    event.srcElement.style.backgroundColor = (event.srcElement.style.backgroundColor === 'black' ? 'white' : 'black'); 
+let lastCell = '';
+
+let stillClicking = false;
+
+// function mouseDown(){
+//   stillClicking = true;
+//   lastCell = 'x'
+//   console.log(stillClicking)
+// }
+
+// function mouseUp(){
+//   stillClicking = false;
+//   lastCell = '';
+//   console.log(stillClicking)
+
+// }
+
+// function hi(){
+//   if(stillClicking){
+//     console.log('hi');
+//   }
+// }
+
+function colorCell(event) {
+  if(lastCell != event.target.id && stillClicking){
+  event.srcElement.style.backgroundColor =
+    event.srcElement.style.backgroundColor === "black" ? "white" : "black";
+  }
+  lastCell = event.target.id;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 var positionRowStart, positionColumnStart;
 var positionRowEnd, positionColumnEnd;
 
+function BFS() {
+  let levels = 1;
+  const NUM_ROWS = 10,
+    NUM_COLUMNS = 10;
 
-function allowDrop(ev) {
-  ev.preventDefault();
-}
+  let delay = 0.5;
 
-function drag(ev) {
-  ev.dataTransfer.setData("text", ev.target.id);
-}
+  let start = `${positionRowStart}-${positionColumnStart}`;
+  let nodesQueue = [start];
+  let toAnimateNodes = [];
+  let visited = {
+    start: true,
+  };
 
-function drop(ev) {
-  ev.preventDefault();
-  var data = ev.dataTransfer.getData("text");
-  ev.target.appendChild(document.getElementById(data));
-}
+  let parent = {};
+  console.log(start)
 
+  const dx = [0, 1, 0, -1];
+  const dy = [-1, 0, 1, 0];
 
+  while (nodesQueue.length) {
+    let n = nodesQueue.length;
 
-for(let i = 2; i <= 32; ++i){
-     document.getElementsByClassName('select-row')[0].innerHTML += `<option>${i}</option>`
-     document.getElementsByClassName('select-column')[0].innerHTML += `<option>${i}</option>`
-}
+    for (let j = 0; j < n; ++j) {
+      let toStartNode = nodesQueue[0];
+      let x = parseInt(toStartNode.substring(0, toStartNode.indexOf("-")));
+      let y = parseInt(
+        toStartNode.substring(toStartNode.indexOf("-") + 1, toStartNode.length)
+      );
+      nodesQueue.shift();
 
-document.getElementById('draw-board').addEventListener('click', constructNewBoard);
+      for (let i = 0; i < 4; ++i) {
+        let changeX = x + dx[i],
+          changeY = y + dy[i];
+        let currentNode = `${changeX}-${changeY}`;
+        if (
+          changeX >= 1 &&
+          changeX <= NUM_ROWS &&
+          changeY >= 1 &&
+          changeY <= NUM_COLUMNS &&
+          visited[currentNode] == undefined &&
+          document.getElementById(currentNode).style.backgroundColor != "black" &&
+          currentNode != start
+        ) {
+          parent[currentNode] = toStartNode;
 
+          toAnimateNodes.push(currentNode);
+          document.getElementById(currentNode).style.backgroundColor =
+            "#49a5d7";
 
-function constructNewBoard(){
-    let table = document.getElementById('board');
-    let numRows = document.getElementsByClassName('select-row')[0].value;
-    let numColumns = document.getElementsByClassName('select-column')[0].value;
-    table.innerHTML = '';
-    for(let i = 1; i <= numRows; ++i){
-        let newRow = document.createElement('tr');
-        for(let j = 1; j <= numColumns; ++j){
-            let newCell = document.createElement('td');
-            newCell.setAttribute('id',  i + '-' + j);
-            newCell.setAttribute('ondrop', 'drop(event)');
-            newCell.setAttribute('ondragover', 'allowDrop(event)');
-            newCell.setAttribute('onclick', 'colorCell(event)')
-            newRow.appendChild(newCell);
+          document.getElementById(
+            currentNode
+          ).style.transitionDelay = `${delay}s`;
+          delay += 0.05;
+
+          if (currentNode == `${positionRowEnd}-${positionColumnEnd}`) {
+            colorShortestPath(toAnimateNodes, parent, start, delay);
+            return;
+          }
+
+          nodesQueue.push(currentNode);
+
+          visited[currentNode] = true;
         }
-        table.appendChild(newRow);
+      }
     }
-
-        positionRowStart = parseInt(1 + Math.random() * (numRows));
-        positionColumnStart = parseInt(1 + Math.random() * (numColumns));
-
-    do{
-        positionRowEnd =  parseInt(1 + Math.random() * (numRows));
-        positionColumnEnd = parseInt(1 + Math.random() * (numColumns));
-    }while(positionColumnStart === positionColumnEnd && positionRowStart === positionRowEnd);
-
-    let startCell = positionRowStart + '-' + positionColumnStart;
-    let endCell = positionRowEnd + '-' + positionColumnEnd;
-
-    let startSymbol = document.createElement('p');
-    startSymbol.innerHTML = 's';
-    startSymbol.setAttribute('draggable', 'true')
-
-    let endSymbol = document.createElement('p');
-    endSymbol.innerHTML = 'e';
-    endSymbol.setAttribute('draggable', 'true')
-
-
-    document.getElementById(startCell).appendChild(startSymbol);
-    document.getElementById(endCell).appendChild(endSymbol);
-
+  }
 }
 
+document.getElementById("bfs").addEventListener("click", BFS);
 
+function colorShortestPath(nodes, parent, start, delay) {
+  let crawl = nodes[nodes.length - 1];
+  let path = [];
+  path.push(crawl);
 
-
-function BFS(){
-
-        let levels = 1;
-
-        console.log('clicked')
-        let numRows = document.getElementsByClassName('select-row')[0].value;
-        let numColumns = document.getElementsByClassName('select-column')[0].value;
-
-        let start = positionRowStart + '-' + positionColumnStart;
-        let nodesQueue = [start];
-        let toAnimateNodes = [];
-        let visited = {
-            start: true
-        }
-
-        const dx = [0, 1, 0, -1];
-        const dy = [-1, 0, 1, 0];
-
-        while(nodesQueue.length){
-
-            let n = nodesQueue.length;
-
-            for(let j = 0; j < n; ++j){
-
-                let currentNode = nodesQueue[0];
-                let x = parseInt(currentNode.substring(0, currentNode.indexOf('-')));
-                let y = parseInt(currentNode.substring(currentNode.indexOf('-') + 1, currentNode.length));
-                nodesQueue.shift();
-
-                for(let i = 0; i < 4; ++i){
-                    let changeX = x + dx[i], changeY = y + dy[i];
-                    let temp = changeX + '-' + changeY;
-                    if(changeX >= 1 && changeX <= numRows && changeY >= 1 && changeY <= numColumns
-                        && visited[temp] == undefined && document.getElementById(temp).style.backgroundColor != 'black') {
-
-                        if(temp == positionRowEnd + '-' + positionColumnEnd){
-                            colorNode(toAnimateNodes);
-                            return;
-                         }
-
-                         nodesQueue.push(temp);
-
-                        document.getElementById(temp).innerHTML = levels;
-                        visited[temp] = true;
-                    }
-                }
-
-
-            }
-
-            
-            levels++;
-        }  
+  while (parent[crawl] !== start) {
+    path.push(parent[crawl]);
+    crawl = parent[crawl];
+  }
+  path.push(crawl);
+  path = path.reverse();
+  setTimeout(() => {
+    path.forEach((node) => {
+      document.getElementById(node).style.backgroundColor = "#fffe6a";
+    });
+  }, delay * 10);
 }
 
-document.getElementById('bfs').addEventListener('click', BFS);
+document.getElementById("dfs").addEventListener("click", () => {
+  let delay = 1;
+  let table = document.getElementById("board");
+  let time = 1;
+  const visited = {};
+  const dx = [1, -1, 0, 0];
+  const dy = [0, 0, -1, 1];
+  let row = positionRowStart,
+    column = positionColumnStart;
 
+  makeDFS(row, column, dx, dy, table, visited, delay);
+});
 
+function makeDFS(row, column, dx, dy, table, visited, delay) {
+  const NUM_ROWS = 10,
+    NUM_COLUMNS = 10;
+  visited[`${row}-${column}`] = true;
+  if (visited[positionRowEnd + "-" + positionColumnEnd] == true) return;
 
+  for (let i = 0; i < 4; ++i) {
+    const x = row + dx[i],
+      y = column + dy[i];
+    const currentNode = `${x}-${y}`;
+    if (
+      x >= 1 &&
+      x <= NUM_ROWS &&
+      y >= 1 &&
+      y <= NUM_COLUMNS &&
+      visited[currentNode] === undefined
+    ) {
+      document.getElementById(currentNode).style.backgroundColor =
+      "#49a5d7";
 
-
-
-function colorNode(nodes){
-    
-    for(let node of nodes){
-         setTimeout(() => {
-            document.getElementById(node).style.backgroundColor = '#40cee3';
-        }, 0);
+    document.getElementById(
+      currentNode
+    ).style.transitionDelay = `${delay}s`;
+    delay += 0.5;
+      makeDFS(row + dx[i], column + dy[i], dx, dy, table, visited);
     }
+  }
 }
 
-
-
-
-
-
-
-document.getElementById('dfs').addEventListener('click', () => {
-
-    let table = document.getElementById('board');
-    let time = 1;
-    const visited = {};
-    const dx = [1, -1, 0, 0];
-    const dy = [0, 0, -1, 1];
-    let row = positionRowStart, column = positionColumnStart
-
-    makeDFS(row, column, dx, dy, table, visited)
-
-})
-
-function makeDFS(row, column, dx, dy, table, visited){
-
-    console.log(row, column)
-    visited[row + '-' + column] = true;
-    let numRows = document.getElementsByClassName('select-row')[0].value;
-    let numColumns = document.getElementsByClassName('select-column')[0].value;
-    if(visited[positionRowEnd + '-' + positionColumnEnd] == true)
-        return;
-
-
-    for(let i = 0; i < 4; ++i){
-
-        if(  row + dx[i] >= 1 && row + dx[i] <=  numRows && column + dy[i] >= 1 && column + dy[i] <= numColumns &&  visited[(row + dx[i]) + '-' + (column + dy[i])] === undefined){
-            makeDFS(row + dx[i], column + dy[i], dx, dy, table, visited)
-        }
+function initializeBoard() {
+  let table = document.getElementById("board");
+  const NUM_ROWS = 10,
+    NUM_COLUMNS = 10;
+  table.innerHTML = "";
+  for (let i = 1; i <= NUM_ROWS; ++i) {
+    let newRow = document.createElement("tr");
+    for (let j = 1; j <= NUM_COLUMNS; ++j) {
+      let newCell = document.createElement("td");
+      newCell.classList.add('empty')
+      newCell.setAttribute("id", i + "-" + j);
+      newCell.setAttribute("onclick", "colorCell(event)");
+      newCell.setAttribute("onmousemove", "colorCell(event)");
+      newCell.setAttribute("onmouseup", "mouseUp()");
+      newCell.setAttribute("onmousedown", "mouseDown()");
+      newRow.appendChild(newCell);
     }
+    table.appendChild(newRow);
+  }
 
+  positionRowStart = parseInt(1 + Math.random() * NUM_ROWS);
+  positionColumnStart = parseInt(1 + Math.random() * NUM_COLUMNS);
+
+  do {
+    positionRowEnd = parseInt(1 + Math.random() * NUM_ROWS);
+    positionColumnEnd = parseInt(1 + Math.random() * NUM_COLUMNS);
+  } while (
+    positionColumnStart === positionColumnEnd &&
+    positionRowStart === positionRowEnd
+  );
+
+  let startCell = positionRowStart + "-" + positionColumnStart;
+  let endCell = positionRowEnd + "-" + positionColumnEnd;
+
+    let sourceImg = document.createElement('img');
+    sourceImg.setAttribute('src', 'source.png')
+    sourceImg.classList.add('source')
+
+    let targetImg = document.createElement('img');
+    targetImg.setAttribute('src', 'target.png')
+    targetImg.classList.add('target')
+
+  document.getElementById(startCell).appendChild(sourceImg);
+  document.getElementById(endCell).appendChild(targetImg);
+  
 }
+
+initializeBoard();
+
+
+
+
+const fill = document.querySelector('.source');
+const empties = document.querySelectorAll('.empty');
+
+// Fill listeners
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
+
+// Loop through empty boxes and add listeners
+for (const empty of empties) {
+  empty.addEventListener('dragover', dragOver);
+  empty.addEventListener('dragenter', dragEnter);
+  empty.addEventListener('dragleave', dragLeave);
+  empty.addEventListener('drop', dragDrop);
+}
+
+
+
+
+
+
+function dragStart() {
+  setTimeout(() => (this.className = 'invisible'), 0);
+}
+
+function dragEnd(e) {
+  this.className = 'fill';
+  let id = document.getElementsByClassName('fill')[0].parentElement.id
+  console.log(id)
+  id = id.split('-')
+  positionRowStart = id[0], positionColumnStart = id[1]
+  console.log(positionRowStart, positionColumnStart)
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragEnter(e) {
+  e.preventDefault();
+  this.className += ' hovered';
+}
+
+function dragLeave() {
+  this.className = 'empty';
+}
+
+function dragDrop() {
+  this.className = 'empty';
+  this.append(fill);
+}
+
+
+
